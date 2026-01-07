19b9725b3384df9a395852498c4802c906bbf006
spacepotatoes <gtroiano77@gmx.de>
2026-01-07 18:59:00 +0000

    First Commit - Portfolio Base

---
+++ fix-types.d.ts
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+
+declare module '@react-three/fiber' {
+  export * from '@react-three/fiber/dist/react-three-fiber';
+}
+
+declare module '@react-three/drei' {
+  export * from '@react-three/drei/index';
+}
+
+declare module 'three-globe';
